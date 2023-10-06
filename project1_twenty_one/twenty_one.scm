7ea772d50d4a87705ef237f07fb25e5af1646824
AmrAboshaisha <90650510+AmrAboshaisha@users.noreply.github.com>
2023-10-06 19:28:48 +0100

    implemented stop-at-17

--- project1_twenty_one/twenty_one.scm
+++ project1_twenty_one/twenty_one.scm
@@ -36,6 +36,18 @@
 (best-total '(ad as 9h))
 
 
+; Define a strategy procedure stop-at-17 that’s identical to the dealer’s, i.e., takes a
+; card if and only if the total so far is less than 17.
+;strategy function should return a true or false output,
+;which tells whether or not the customer wants another card
+
+(define (stop-at-17 customer-hand-so-far dealer-up-card)
+  (if (< (best-total customer-hand-so-far) 17) #t #f))
+
+;testing
+(stop-at-17 '(ad 5h) (word 'as))
+(stop-at-17 '(ad 5s 5h) (word 'as))
+
 ;----------------------------------------------------------------------------------
 ; starter code
 (define (twenty-one strategy)
